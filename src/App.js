27f74d8d6e50b5959c309619b3893c469524a80b
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-08-29 16:18:48 +0700

    finished header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.scss';
+import { FaHome } from 'react-icons/fa'
 
 function App() {
   return (
     <div className='todo'>
-    <div className="todo__header">Header</div>
-    <div className="todo__sidebar">SideBar</div>
-    <div classNAme="todo__content">TodoContent</div>
-</div>
+      <div className="todo__header">
+        <header className='header'>
+          <div className='header__logo'>
+            <FaHome />
+          </div>
+          <div className='header__text'>TodoList</div>
+          <div className='header__search'>
+            <input />
+          </div>
+        </header>
+      </div>
+      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__content">TodoContent</div>
+    </div>
   )
 }
 
